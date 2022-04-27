bc6a19d86b2eb6c1180e022736d7516e1aa12829
Joe McDonough <joemcdonoughmusic.com>
2022-04-26 22:17:29 -0400

    complete mobile styles

--- version-no-react/js/main.js
+++ version-no-react/js/main.js
@@ -2,6 +2,10 @@ const cardContainer = document.getElementById("card-container");
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("open-modal");
 const closeModal = document.getElementById("close-modal");
+const navExpansionArrowOne = document.getElementById("nav-expansion-arrow-one");
+const navExpansionArrowTwo = document.getElementById("nav-expansion-arrow-two");
+const navExpansionOne = document.getElementById("nav-expansion-one");
+const navExpansionTwo = document.getElementById("nav-expansion-two");
 
 openModal.addEventListener("click", () => {
   modal.classList.remove("hide");
@@ -13,4 +17,34 @@ closeModal.addEventListener("click", () => {
   modal.classList.add("hide");
   openModal.classList.remove("hide");
   cardContainer.classList.remove("blur");
+});
+
+navExpansionArrowOne.addEventListener("click", () => {
+  if (
+    navExpansionArrowOne.getAttribute("src") === "images/icon-arrow-down.svg"
+  ) {
+    navExpansionArrowOne.setAttribute("src", "images/icon-arrow-up.svg");
+    navExpansionArrowOne.setAttribute("alt", "clickable arrow pointing up");
+  } else if (
+    navExpansionArrowOne.getAttribute("src") === "images/icon-arrow-up.svg"
+  ) {
+    navExpansionArrowOne.setAttribute("src", "images/icon-arrow-down.svg");
+    navExpansionArrowOne.setAttribute("alt", "clickable arrow pointing down");
+  }
+  navExpansionOne.classList.toggle("hide");
+});
+
+navExpansionArrowTwo.addEventListener("click", () => {
+  if (
+    navExpansionArrowTwo.getAttribute("src") === "images/icon-arrow-down.svg"
+  ) {
+    navExpansionArrowTwo.setAttribute("src", "images/icon-arrow-up.svg");
+    navExpansionArrowTwo.setAttribute("alt", "clickable arrow pointing up");
+  } else if (
+    navExpansionArrowTwo.getAttribute("src") === "images/icon-arrow-up.svg"
+  ) {
+    navExpansionArrowTwo.setAttribute("src", "images/icon-arrow-down.svg");
+    navExpansionArrowTwo.setAttribute("alt", "clickable arrow pointing down");
+  }
+  navExpansionTwo.classList.toggle("hide");
 });
