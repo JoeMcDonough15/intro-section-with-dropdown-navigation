6f2d0808ff86460edbefb4e4c25b4f8adf0e1cd2
Joe McDonough <joemcdonoughmusic.com>
2022-05-05 00:25:04 -0400

    make mobile navigation and desktop navigation bars, add fixed position to header, correct measurements to design, remove id's and use classes for event listeners on mobile and desktop size navigation arrows and expansions

--- version-no-react/js/main.js
+++ version-no-react/js/main.js
@@ -2,49 +2,62 @@ const blurContainer = document.getElementById("blur");
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("open-modal");
 const closeModal = document.getElementById("close-modal");
-const navExpansionArrowOne = document.getElementById("nav-expansion-arrow-one");
-const navExpansionArrowTwo = document.getElementById("nav-expansion-arrow-two");
-const navExpansionOne = document.getElementById("nav-expansion-one");
-const navExpansionTwo = document.getElementById("nav-expansion-two");
+const navExpansionArrowsFeatures = Array.from(
+  document.getElementsByClassName("nav-expansion-arrow-features")
+);
+
+const navExpansionArrowsCompany = Array.from(
+  document.getElementsByClassName("nav-expansion-arrow-company")
+);
+const navExpansionFeatures = Array.from(
+  document.getElementsByClassName("nav-expansion-features")
+);
+const navExpansionCompany = Array.from(
+  document.getElementsByClassName("nav-expansion-company")
+);
 
 openModal.addEventListener("click", () => {
-  modal.classList.add("nav-menu-appear");
+  modal.classList.add("mobile-nav-menu-appear");
   openModal.classList.add("hide");
   blurContainer.classList.add("blur");
 });
 
 closeModal.addEventListener("click", () => {
-  modal.classList.remove("nav-menu-appear");
+  modal.classList.remove("mobile-nav-menu-appear");
   openModal.classList.remove("hide");
   blurContainer.classList.remove("blur");
 });
 
-navExpansionArrowOne.addEventListener("click", () => {
-  if (
-    navExpansionArrowOne.getAttribute("src") === "images/icon-arrow-down.svg"
-  ) {
-    navExpansionArrowOne.setAttribute("src", "images/icon-arrow-up.svg");
-    navExpansionArrowOne.setAttribute("alt", "clickable arrow pointing up");
-  } else if (
-    navExpansionArrowOne.getAttribute("src") === "images/icon-arrow-up.svg"
-  ) {
-    navExpansionArrowOne.setAttribute("src", "images/icon-arrow-down.svg");
-    navExpansionArrowOne.setAttribute("alt", "clickable arrow pointing down");
-  }
-  navExpansionOne.classList.toggle("hide");
+navExpansionArrowsFeatures.forEach((featuresArrow) => {
+  featuresArrow.addEventListener("click", () => {
+    if (featuresArrow.getAttribute("src") === "images/icon-arrow-down.svg") {
+      featuresArrow.setAttribute("src", "images/icon-arrow-up.svg");
+      featuresArrow.setAttribute("alt", "clickable arrow pointing up");
+    } else if (
+      featuresArrow.getAttribute("src") === "images/icon-arrow-up.svg"
+    ) {
+      featuresArrow.setAttribute("src", "images/icon-arrow-down.svg");
+      featuresArrow.setAttribute("alt", "clickable arrow pointing down");
+    }
+    navExpansionFeatures.forEach((featureExpansion) => {
+      featureExpansion.classList.toggle("hide");
+    });
+  });
 });
 
-navExpansionArrowTwo.addEventListener("click", () => {
-  if (
-    navExpansionArrowTwo.getAttribute("src") === "images/icon-arrow-down.svg"
-  ) {
-    navExpansionArrowTwo.setAttribute("src", "images/icon-arrow-up.svg");
-    navExpansionArrowTwo.setAttribute("alt", "clickable arrow pointing up");
-  } else if (
-    navExpansionArrowTwo.getAttribute("src") === "images/icon-arrow-up.svg"
-  ) {
-    navExpansionArrowTwo.setAttribute("src", "images/icon-arrow-down.svg");
-    navExpansionArrowTwo.setAttribute("alt", "clickable arrow pointing down");
-  }
-  navExpansionTwo.classList.toggle("hide");
+navExpansionArrowsCompany.forEach((companyArrow) => {
+  companyArrow.addEventListener("click", () => {
+    if (companyArrow.getAttribute("src") === "images/icon-arrow-down.svg") {
+      companyArrow.setAttribute("src", "images/icon-arrow-up.svg");
+      companyArrow.setAttribute("alt", "clickable arrow pointing up");
+    } else if (
+      companyArrow.getAttribute("src") === "images/icon-arrow-up.svg"
+    ) {
+      companyArrow.setAttribute("src", "images/icon-arrow-down.svg");
+      companyArrow.setAttribute("alt", "clickable arrow pointing down");
+    }
+    navExpansionCompany.forEach((companyExpansion) => {
+      companyExpansion.classList.toggle("hide");
+    });
+  });
 });
